32c074e3a8b23df4efa12a8014df8bfc90b0c42e
andrea2301 <andreaospino323@gmail.com>
2025-11-12 14:01:49 -0500

    Router Fix

--- src/Routes/AppRouter.jsx
+++ src/Routes/AppRouter.jsx
@@ -1,6 +1,6 @@
 // src/Routes/AppRouter.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "../pages/Home/Home";
+import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 
@@ -8,12 +8,29 @@ function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home/>} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default AppRouter;
+
+// import { Routes, Route } from "react-router";
+
+// function Wizard() {
+//   return (
+//     <div>
+//       <h1>Some Wizard with Steps</h1>
+//       <Routes>
+//         <Route index element={<StepOne />} />
+//         <Route path="step-2" element={<StepTwo />} />
+//         <Route path="step-3" element={<StepThree />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
+
 // src/pages/Login.jsx
 function Login() {
+
+  const navigate = useNavigate();
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    // Aquí iría la lógica de autenticación
+    // Si la autenticación es exitosa, redirigir a la página principal
+    navigate("/home");
+  }
+
+
   return (
     <div className="login-container">
       <h1>View login</h1>
       <form>
         <input type="text" placeholder="Usuario" />
         <input type="password" placeholder="Contraseña" />
-        <button type="submit">Entrar</button>
+        <button onClick={handleLogin} type="submit">Entrar</button>
       </form>
     </div>
   );
